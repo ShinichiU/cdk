c8d1e927d2e853bb5ed5e24cd0cb56a366861d13
卜部真一 <urabe@nuts-choco.com>
2024-11-09 17:49:43 +0900

    [MOD] encryptShar256 の引数名を変更

--- lib/util/encrypt.ts
+++ lib/util/encrypt.ts
@@ -1,7 +1,8 @@
 import { createHash } from 'crypto';
 
-export const encryptSha256 = (string: string, substr?: number): string => {
+export const encryptSha256 = (string: string, length?: number): string => {
   const hash = createHash('sha256');
+  const defaultLength = 10;
   hash.update(string);
-  return hash.digest('hex').substring(0, substr ?? 10);
+  return hash.digest('hex').substring(0, length ?? defaultLength);
 };
